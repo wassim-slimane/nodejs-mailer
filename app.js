d59629afa95dfdcd0273571caf0508a9078ddfd9
wassim-slimane <wassim.slimane01@gmail.com>
2024-12-02 19:59:04 +0100

    body parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
 
 // Constants
 const app = express();
@@ -7,13 +8,15 @@ const port = 3000;
 
 // Middleware
 app.use(cors());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 // Routes
 app.get('/', (req, res, next) => {
     res.send('Hello Wash!')
 })
 app.post('/contact', (req, res) => {
-    res.send('Got a POST request');
+    console.log(req.body)
 })
 
 app.listen(port, () => {
