e3778aedbc72558a947bb73507a0d55a88b2ce2f
wassim <wassim.slimane01@gmail.com>
2024-12-11 23:01:08 +0100

    Use the new declaration of the transporter

--- checkConnection.js
+++ checkConnection.js
@@ -1,16 +1,5 @@
 require("dotenv").config()
-const nodemailer = require('nodemailer');
-
-// Create a transporter object
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: process.env.EMAIL_SECURE === 'true', // use SSL
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
+const {transporter} = require('./transporter');
 
 // Verify smtp connection
 function checkConnection() {
@@ -19,10 +8,8 @@ function checkConnection() {
             console.log('Connection error: ' + error);
         } else {
             console.log(success);
-            //sendEmail().catch((error) => console.log(error));
         }
     })
 }
 
-//checkConnection(transporter);
 module.exports = {checkConnection};
--- mailer.js
+++ mailer.js
@@ -1,16 +1,6 @@
 require("dotenv").config()
 const nodemailer = require('nodemailer');
-
-// Create a transporter object
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: process.env.EMAIL_SECURE === 'true', // use SSL
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
+const {transporter} = require('./transporter');
 
 async function sendEmail(form) {
     const name = form.name.toString().toLowerCase().replace(/\s/g, '');
