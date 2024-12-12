15682bd213ed388e27a28238610d63b89233c0a6
wassim <wassim.slimane01@gmail.com>
2024-12-12 13:46:39 +0100

    Using a template instead of text in the email options

--- mailer.js
+++ mailer.js
@@ -1,13 +1,30 @@
 require("dotenv").config()
+const fs = require('fs');
+const handlebars = require('handlebars');
+
 const {transporter} = require('./transporter');
 
 async function sendEmail(form) {
-    const name = form.name.toString().toLowerCase().replace(/\s/g, '');
+    // Data for the template
+    const context = {
+        subject: form.subject,
+        name: form.name,
+        email: form.email,
+        message: form.message,
+    };
+
+    // Generate html template to send with data
+    const htmlSource = fs.readFileSync('./template.html', 'utf-8');
+    const htmlTemplate = handlebars.compile(htmlSource);
+    const htmlToSend = htmlTemplate(context);
+
+    // Set mail options
     const mailOptions = {
         from: process.env.EMAIL_FROM,
         to: process.env.EMAIL_TO,
         subject: 'EMAIL FROM PORTFOLIO',
         text: `Subject: ${form.subject}, Name: ${name}, Email: ${form.email}, Message: ${form.message}`,
+        html: htmlToSend
     };
 
     return await transporter.sendMail(mailOptions);
